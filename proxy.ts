c116e5346f4ed8d0b8d116b38d1fb60e6d57453f
ookk11711 <121254019+ookk11711@users.noreply.github.com>
2025-11-30 07:48:25 +0800

    Add files via upload

---
+++ proxy.ts
@@ -0,0 +1,22 @@
+const AnyProxy = require("anyproxy");
+
+const options = {
+  port: process.env.PORT || 8000,
+  rule: null,
+  webInterface: false,
+  throttle: 10000,
+  forceProxyHttps: true,
+  silent: false,
+};
+
+const proxyServer = new AnyProxy.ProxyServer(options);
+
+proxyServer.on("ready", () => {
+  console.log("Proxy server is running...");
+});
+
+proxyServer.on("error", (e: any) => {
+  console.error("Proxy server error:", e);
+});
+
+proxyServer.start();
